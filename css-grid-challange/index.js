e53a6d25b386b8c95a469fd5da4ac3d96481f65b
mi8722663079 <mi8722663@gmail.com>
2023-08-17 09:54:38 +0300

    prettier

--- css-grid-challange/index.js
+++ css-grid-challange/index.js
@@ -1,7 +1,6 @@
-
-function navrell(){
-  const parent = document.querySelector('.grandP');
-  const nav = document.querySelector('nav');
-  parent.classList.toggle('grid-expand');
-  nav.classList.toggle('hide-nav');
+function navrell() {
+  const parent = document.querySelector(".grandP");
+  const nav = document.querySelector("nav");
+  parent.classList.toggle("grid-expand");
+  nav.classList.toggle("hide-nav");
 }
